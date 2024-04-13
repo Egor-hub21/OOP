a29bfcd457fd7742fdc2d1d58685e4bd9a4b917e
Egor Kichigin <eek2077@outlook.com>
2024-04-14 00:35:58 +0700

    add(Lab3): ConsoleLoader и GeometricShapes

---
+++ ConsoleLoader/Program.cs
@@ -0,0 +1,17 @@
+namespace ConsoleLoader
+{
+    /// <summary>
+    /// Класс Program.
+    /// </summary>
+    internal class Program
+    {
+
+        /// <summary>
+        /// Метод Main.
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Привет");
+        }
+    }
+}
